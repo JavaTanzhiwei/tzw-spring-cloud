9212f0f61304bd05d27b1f07d4863c3e0ff9b2e6
tanzhiwei <tanzhiwei0526@163.com>
2022-10-13 17:23:00 +0800

    Po类

---
+++ tzw-api/tzw-api-system/src/main/java/com/springcloud/study/api/system/bean/po/SysDictData.java
@@ -0,0 +1,13 @@
+package com.springcloud.study.api.system.bean.po;
+
+import com.springcloud.study.core.base.BasePo;
+import lombok.Data;
+
+/**
+ * 类 描 述: 字典类型表 sys_dict_type
+ * 作   者: 谭志伟
+ * 时   间: 2022/10/13  17:21
+ */
+@Data
+public class SysDictData extends BasePo {
+}
---
+++ tzw-api/tzw-api-system/src/main/java/com/springcloud/study/api/system/bean/po/SysDictType.java
@@ -0,0 +1,13 @@
+package com.springcloud.study.api.system.bean.po;
+
+import com.springcloud.study.core.base.BasePo;
+import lombok.Data;
+
+/**
+ * 类 描 述: 字典数据表 sys_dict_data
+ * 作   者: 谭志伟
+ * 时   间: 2022/10/13  17:20
+ */
+@Data
+public class SysDictType extends BasePo {
+}
