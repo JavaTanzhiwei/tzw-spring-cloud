e5a6295e1e9c0cb2825c9b132de9ba8b15c3a642
tanzhiwei <tanzhiwei0526@163.com>
2022-10-18 13:37:16 +0800

    缓存工具类

---
+++ tzw-common/tzw-common-security/src/main/java/com/springcloud/study/security/utils/DictUtils.java
@@ -0,0 +1,51 @@
+package com.springcloud.study.security.utils;
+
+import com.springcloud.study.api.system.bean.po.SysDictData;
+import com.springcloud.study.core.constant.CacheConstants;
+import com.springcloud.study.redis.utils.CacheUtils;
+
+import java.util.List;
+
+/**
+ * 类 描 述: 字典工具类
+ * 作   者: 谭志伟
+ * 时   间: 2022/10/18  11:35
+ */
+public class DictUtils {
+
+    /**
+     * 设置字典缓存
+     *
+     * @param key       参数键
+     * @param dictDatas 字典数据列表
+     */
+    public static void setDictCache(String key, List<SysDictData> dictDatas) {
+        CacheUtils.put(CacheConstants.SYS_DICT_KEY, key, dictDatas);
+    }
+
+    /**
+     * 获取字典缓存
+     *
+     * @param key 参数键
+     * @return dictDatas 字典数据列表
+     */
+    public static List<SysDictData> getDictCache(String key) {
+        return CacheUtils.get(CacheConstants.SYS_DICT_KEY, key);
+    }
+
+    /**
+     * 删除指定字典缓存
+     *
+     * @param key 字典键
+     */
+    public static void removeDictCache(String key) {
+        CacheUtils.evict(CacheConstants.SYS_DICT_KEY, key);
+    }
+
+    /**
+     * 清空字典缓存
+     */
+    public static void clearDictCache() {
+        CacheUtils.clear(CacheConstants.SYS_DICT_KEY);
+    }
+}
